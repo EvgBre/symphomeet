4cb112ff9299ffc35d8cd39cc178e0f65c481129
Evan Breland <evan.breland@gmail.com>
2023-03-04 12:37:27 -0600

    ad card and form added and route pages fixed

---
+++ components/AdCard.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteAd } from '../api/adData';
+
+function AdCard({ adObj, onUpdate }) {
+  const deleteThisAd = () => {
+    if (window.confirm(`Delete ${adObj.name}?`)) {
+      deleteAd(adObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Body>
+        <Card.Title>{adObj.name}</Card.Title>
+        <p className="card-text bold">{adObj.chamber}</p>
+        <Link href={`/ad/${adObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/ad/edit/${adObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisAd} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+AdCard.propTypes = {
+  adObj: PropTypes.shape({
+    chamber: PropTypes.string,
+    name: PropTypes.string,
+    public: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default AdCard;
